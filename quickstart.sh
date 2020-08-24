837397a94a098d132d83eafe285b8fa0deca3665
ayunami2000 <spwilliamsiam@gmail.com>
2020-08-24 10:16:35 -0400

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,5 @@
 
-curl -L -o /tmp/fapt-install.sh https://github.com/gabrielgrant/fapt/raw/master/fapt-install.sh;
-curl -L -o /tmp/fapt.sh https://github.com/gabrielgrant/fapt/raw/master/fapt.sh;
+curl -L -o /tmp/fapt-install.sh https://github.com/ayunami2000/fapt/raw/master/fapt-install.sh;
+curl -L -o /tmp/fapt.sh https://github.com/ayunami2000/fapt/raw/master/fapt.sh;
 
 source /tmp/fapt-install.sh;
